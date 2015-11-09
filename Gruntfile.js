9ec6f5e9c3e75930bfe2dc5f3cae15e63b3d1fc4
weizlini <Jomo@Willy.local>
2015-11-08 21:25:15 -0500

    working demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,19 @@ module.exports = function(grunt) {
                     dest: 'demo/audio',
                     flatten:true
                 },{
+                    expand: true,
+                    cwd: 'src/assets/fonts',
+                    src: ['**/*'],
+                    dest: 'demo/fonts',
+                    flatten:true
+                }
+                ,{
+                    expand: true,
+                    cwd: 'src/assets/images',
+                    src: ['**/*.jpg','**/*.png','**/*.gif'],
+                    dest: 'demo/images',
+                    flatten:true
+                },{
                   expand: true,
                     cwd: 'bower_components/jquery/dist',
                     src: ['jquery.min.js','jquery.min.map'],
@@ -27,6 +40,8 @@ module.exports = function(grunt) {
                     dest: 'demo/js'
                 },{
                   'demo/index.html':'src/test.html'
+                },{
+                  'demo/js/sruti.js':'src/assets/js/sruti.js'
                 }
                 ]
             }
@@ -48,15 +63,24 @@ module.exports = function(grunt) {
                 ext:'.css'
               }]
             }
+          },
+        connect: {
+          demo: {
+            options: {
+              port: 1976,
+              base: 'demo'
+            }
           }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.registerTask('default', ['copy']);
-    grunt.registerTask('dev', ['copy','uglify','sass','watch']);
+    grunt.registerTask('dev', ['copy','uglify','sass','connect','watch']);
     grunt.registerTask('build',['copy','uglify','sass']);
     
 };
--- src/assets/js/sruti.js
+++ src/assets/js/sruti.js
@@ -1,3 +1,196 @@
+
+//var audiocontext = new webkitAudioContext();
 var sruti = {
-  
+  samples:[
+'01_c1.mp3',
+'02_c1_sh.mp3',
+'03_d1.mp3',
+'04_d1_sh.mp3',
+'05_e1.mp3',
+'06_f1.mp3',
+'07_f1_sh.mp3',
+'08_g1.mp3',
+'09_g1_sh.mp3',
+'10_a1.mp3',
+'11_a1_sh.mp3',
+'12_b1.mp3',
+'13_c2.mp3',
+'14_c2_sh.mp3',
+'15_d2.mp3',
+'16_d2_sh.mp3',
+'17_e2.mp3',
+'18_f2.mp3',
+'19_f2_sh.mp3',
+'20_g2.mp3',
+'21_g2_sh.mp3',
+'22_a2.mp3',
+'23_a2_sh.mp3',
+'24_b2.mp3',
+'25_c3.mp3',
+  ],
+  AudioContext:null,
+  source:[],
+  init:function(){
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+     
+     this.AudioContext = new window.AudioContext();
+     this.loadSamples();
+     //this.test();
+     
+  },
+  initUI:function(){
+    $('.sruti-box').show();
+    $( ".note-picker" ).slider({
+      value:0,
+      min: 0,
+      max: 12,
+      step: 1,
+      slide: function( event, ui ) {
+        sruti.tampuraKey = ui.value;
+        if(sruti.playing)
+        {
+        sruti.stop();
+        sruti.play();
+        }
+      }
+    });
+    $( ".tempo-slider" ).slider({
+      value:60,
+      min: 40,
+      max: 120,
+      step: 1,
+      slide: function( event, ui ) {
+        sruti.tempo = ui.value;
+        if(sruti.playing)
+        {
+        sruti.stop();
+        sruti.play();
+        }
+      }
+    });
+    
+    $( ".start" ).click(function(){
+      //sruti.stop();
+      if(!sruti.playing)
+      {
+        sruti.play();
+      }
+      $(this).addClass('on');
+      
+    });
+    $( ".stop" ).click(function(){
+      //sruti.stop();
+      sruti.stop();
+      $('.start').removeClass('on');
+    });
+  },
+  debug:true,
+  log:function(text){
+    if(this.debug)
+    {
+        console.log('-----------------------------------');
+        console.dir(text);
+    }
+  },
+  _loadedSamples:0,
+  loadSample:function(path,source){
+        sruti.log('about to load '+path);
+    var request = new XMLHttpRequest();
+        request.open('GET', 'audio/'+path, true);
+        request.responseType = "arraybuffer";
+        request.onload = function() {
+                
+                sruti.log(path+' loaded');
+                sruti.AudioContext.decodeAudioData(request.response, function(buffer) {
+                
+                
+                source.buffer = buffer;
+                source.connect(sruti.AudioContext.destination);
+                source.loop=true;                      //start(0) should play asap.
+                //source.start(0);
+                sruti._loadedSamples++;
+                if(sruti._loadedSamples == sruti.samples.length)
+                {
+                    sruti._onSamplesLoad();
+                }
+            });
+        };
+        request.onerror = function (e) {
+          console.error(request.statusText);
+        };
+      
+          
+        request.send();
+        sruti.log('request sent');
+  },
+  loadSamples:function(){
+      var toLoad = sruti.samples.length;
+      this.samples.forEach(function(val,index){
+        
+        sruti.log('loading '+val);
+        sruti.source[index] = sruti.AudioContext.createBufferSource();
+        sruti.loadSample(val,sruti.source[index]);
+     
+      });
+  },
+  _onSamplesLoad:function(){
+    //alert('samples loaded');
+    this.selectKey(0);
+    this.initUI();
+    //this.play();
+  },
+  tampuraKey:0,
+  tampuraDrone:[],
+  selectKey:function(note)
+  {
+      
+      if(note>11)
+        note =0;
+       if(note<0)
+        note =0;
+        this.tampuraKey = note;
+        
+  },
+  tempo:60,
+  loopTimeout:null,
+  playing:false,
+  play:function(){
+    //var length = 60/this.tempo;
+    //var startTime = context.currentTime+1;
+        this.playing = true;
+        var tampuraDrone = [];
+        tampuraDrone[0] = sruti.AudioContext.createBufferSource();
+        tampuraDrone[0].buffer = this.source[this.tampuraKey+7].buffer;
+        tampuraDrone[0].connect(sruti.AudioContext.destination);
+        tampuraDrone[1] = sruti.AudioContext.createBufferSource();
+        tampuraDrone[1].buffer = this.source[this.tampuraKey+12].buffer;
+        tampuraDrone[1].connect(sruti.AudioContext.destination);
+        tampuraDrone[2] = sruti.AudioContext.createBufferSource();
+        tampuraDrone[2].buffer = this.source[this.tampuraKey+12].buffer;
+        tampuraDrone[2].connect(sruti.AudioContext.destination);
+        tampuraDrone[3] = sruti.AudioContext.createBufferSource();
+        tampuraDrone[3].buffer = this.source[this.tampuraKey].buffer;
+        tampuraDrone[3].connect(sruti.AudioContext.destination);
+        this.tampuraDrone = tampuraDrone;
+        var start =sruti.AudioContext.currentTime;
+        
+        
+        tampuraDrone[0].start(start+60/sruti.tempo*0);
+        tampuraDrone[1].start(start+60/sruti.tempo*2);
+        tampuraDrone[2].start(start+60/sruti.tempo*3);
+        tampuraDrone[3].start(start+60/sruti.tempo*4);
+        sruti.loopTimeout = setTimeout(function(){sruti.play();},60/sruti.tempo*6*1000);
+    
+  },
+  stop:function(){
+    this.playing = false;
+    clearTimeout(sruti.loopTimeout);
+        this.tampuraDrone[0].stop();
+        this.tampuraDrone[1].stop();
+        this.tampuraDrone[2].stop();
+        this.tampuraDrone[3].stop();
+  }
 };
+$(function(){
+  sruti.init();
+});
